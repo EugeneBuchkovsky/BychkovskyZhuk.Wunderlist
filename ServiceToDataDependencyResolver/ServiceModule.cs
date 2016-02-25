649b85dfe70f569dfa9f43dfd263414ba2e13e07
EugeneBuchkovsky <buchkovsyeugene20th@gmail.com>
2016-02-26 01:41:56 +0200

    Add service to data dependency resolver

---
+++ ServiceToDataDependencyResolver/ServiceModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wunderlist.DataAccess.Interfaces.Interfaces;
+using Wunderlist.DataAccess.Repositories;
+using Ninject.Modules;
+
+namespace ServiceToDataDependencyResolver
+{
+    public class ServiceModule : NinjectModule
+    {
+        private string connectionString;
+
+        public ServiceModule(string connection)
+        {
+            this.connectionString = connection;
+        }
+
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<UnitOfWork>().WithConstructorArgument(connectionString);
+        }
+    }
+}
